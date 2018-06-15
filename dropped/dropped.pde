a4f8693d63d1436da2b86f465ce77f7fe3ea3d46
FruitWave <league@WorthyMini-5.attlocal.net>
2018-06-14 20:01:40 -0700

    aidanslore

--- dropped/dropped.pde
+++ dropped/dropped.pde
@@ -66,3 +66,4 @@ void stepSort(int[] arr) {
     }
   }
 }
+c
--- src/_00_IntroToArrays/_00_ArrayCheatSheet.java
+++ src/_00_IntroToArrays/_00_ArrayCheatSheet.java
@@ -2,32 +2,65 @@ package _00_IntroToArrays;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
-		//1. make an array of 5 Strings
+		// 1. make an array of 5 Strings
+		String[] nowyouhavetosaythis = new String[5];
+		nowyouhavetosaythis[0] = "okay";
+		nowyouhavetosaythis[1] = "play";
+		nowyouhavetosaythis[2] = "wayy";
+		nowyouhavetosaythis[3] = "neigh";
+		nowyouhavetosaythis[4] = "stay";
+		// 2. print the third element in the array
+		System.out.println(nowyouhavetosaythis[2]);
+		// 3. set the third element to a different value
+		nowyouhavetosaythis[2] = "nope";
+		// 4. print the third element again
+		System.out.println(nowyouhavetosaythis[2]);
+		// 5. use a for loop to set all the elements in the array to a string of your
+		// choice
+		for (int i = 0; i < nowyouhavetosaythis.length; i++) {
+			String poh = JOptionPane.showInputDialog("Set String " + i);
+			nowyouhavetosaythis[i] = poh;
+		}
+		// 6. use a for loop to print all the values in the array
+		// BE SURE TO USE THE ARRAY'S length VARIABLE
+		for (int i = 0; i < nowyouhavetosaythis.length; i++) {
+			System.out.println(nowyouhavetosaythis[i]);
+		}
+		// 7. make an array of 50 integers
+		Integer[] intdex = new Integer[50];
+		// 8. use a for loop to make every value of the integer array a random number
+		for (int k = 0; k < intdex.length; k++) {
+			intdex[k] = new Random().nextInt();
+		}
+		// 9. without printing the entire array, print only the smallest number on the
+		// array
+		for (int i = 0; i < intdex.length; i++) {
 
-		//2. print the third element in the array
+			for (int j = 0; j < intdex.length - 1; j++) {
 
-		//3. set the third element to a different value
+				if (intdex[j] > intdex[j + 1]) {
+					int temp = intdex[j + 1];
+					intdex[j+1] = intdex[j];
+					
+					// init = finale;
+					intdex[j] = temp;
+					// finale = init;
+				}
+			}
+		}
+		// 10 print the entire array to see if step 8 was correct
+		for (
 
-		//4. print the third element again
-		
-		//5. use a for loop to set all the elements in the array to a string of your choice
-		
-		//6. use a for loop to print all the values in the array
-		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
-		//7. make an array of 50 integers
+				int i = 0; i < intdex.length; i++) {
+			System.out.println(intdex[i]);
+		}
+		// 11. print the largest number in the array.
 
-		//8. use a for loop to make every value of the integer array a random number
+		// 12. print only the last element in the array
 
-		//9. without printing the entire array, print only the smallest number on the array
-
-		//10 print the entire array to see if step 8 was correct
-
-		//11. print the largest number in the array.
-		
-		//12. print only the last element in the array
-		
 	}
 }
